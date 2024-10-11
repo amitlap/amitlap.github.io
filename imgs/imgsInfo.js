f62f58d724efa655e6ae83af1d0cb4da9e1335b5
amitlap <alapid@clearion.com>
2024-10-11 19:51:43 +0300

    Add info file

---
+++ imgs/imgsInfo.js
@@ -0,0 +1,3 @@
+const allImgs = {
+    img1: 'path to img 1'
+};
---
+++ info/info.js
@@ -0,0 +1,19 @@
+const headersInfoDictionary = {
+  VanBurenProject: 'Van Buren Project',
+  FentonProject: 'Fenton Project',
+  FentonCity: 'Fenton City',
+  AnnArborCity: 'Ann Arbor City',
+}
+
+const locationsImgsInfo = {
+  default: {
+    img1 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg',
+    img2 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg',
+    img3 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg'
+  },
+  fentonProject: {
+    img1 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg',
+    img2 : 'https://brookwood.co.il/wp-content/uploads/2022/10/fenton-demo.jpg',
+    img3 : 'https://img1.wsimg.com/isteam/ip/92853d7f-dbb3-44d8-978d-c189986debc2/Brookwood%20Estates%20-%20Van%20Buren%2022.jpg/:/rs=w:1300,h:800'
+  }
+}
