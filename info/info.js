122324fc7c2399b0ecdfad1164684e58bcf14f31
amitlap <alapid@clearion.com>
2024-10-13 15:33:55 +0300

    Add text panel

--- info/info.js
+++ info/info.js
@@ -12,8 +12,16 @@ const locationsImgsInfo = {
     img3 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg'
   },
   fentonProject: {
-    img1 : 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg',
-    img2 : 'https://brookwood.co.il/wp-content/uploads/2022/10/fenton-demo.jpg',
-    img3 : 'https://img1.wsimg.com/isteam/ip/92853d7f-dbb3-44d8-978d-c189986debc2/Brookwood%20Estates%20-%20Van%20Buren%2022.jpg/:/rs=w:1300,h:800'
+    img1: 'https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg',
+    img2: 'https://brookwood.co.il/wp-content/uploads/2022/10/fenton-demo.jpg',
+    img3: 'https://img1.wsimg.com/isteam/ip/92853d7f-dbb3-44d8-978d-c189986debc2/Brookwood%20Estates%20-%20Van%20Buren%2022.jpg/:/rs=w:1300,h:800',
+    img4: 'https://img1.wsimg.com/isteam/ip/92853d7f-dbb3-44d8-978d-c189986debc2/Brookwood%20Estates%20ALL%20UNITS.jpg/:/rs=w:1300,h:800',
   }
 }
+
+const textContentInfo = {
+  fentonProject: {
+    wantedText: 'This development will begin breaking ground in summer of 2022. Rolling hills, large wooded area, secluded walking trails are just some of the attractions of this coming development.',
+    moreText: 'Located just half a mile from Fenton Lake and 2 miles north of the vibrant downtown Fenton area.'
+  }
+} 
