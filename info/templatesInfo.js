ac38a481608423b42f5176a6808ca5c0c6be77cc
amitlap <alapid@clearion.com>
2024-10-26 08:32:02 +0300

    Make tabs reuseable

---
+++ info/templatesInfo.js
@@ -0,0 +1,93 @@
+// info.js
+
+const tabsInfo = {
+    FentonProject: {
+      header: `Fenton Project`,
+      overview: `
+        <p>This is the overview for the Fenton project. This development will begin breaking ground in summer of 2022.</p>
+        <a href="https://brookwood.co.il/project/brookwood-estates-fenton" target="_blank" rel="noopener noreferrer">View Fenton project page</a>
+      `,
+      photos: `
+        <div id="carousel1" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://brookwood.co.il/wp-content/uploads/2022/09/Fenton_Units_color.jpg" class="d-block w-100" alt="fenton-demo">
+            </div>
+            <div class="carousel-item">
+              <img src="https://brookwood.co.il/wp-content/uploads/2022/10/fenton-demo.jpg" class="d-block w-100" alt="fenton-demo">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carousel1" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carousel1" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      `,
+      details: `
+        <p>Details about the Fenton project. This area is known for its rolling hills, wooded trails, and proximity to downtown Fenton.</p>
+      `
+    },
+    SpringMeadowsCountryClub: {
+      header: `Fenton Farms Golf Club`,
+      overview: `
+        <p>This is the overview for the Spring Meadows Country Club project. Golf Club 3km.</p>
+      `,
+      photos: `
+        <div id="carousel2" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://example.com/brookwood1.jpg" class="d-block w-100" alt="brookwood-demo">
+            </div>
+            <div class="carousel-item">
+              <img src="https://example.com/brookwood2.jpg" class="d-block w-100" alt="brookwood-demo">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carousel2" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carousel2" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      `,
+      details: `
+        <p>This is the overview for the Spring Meadows Country Club project. Golf Club 3km
+        This is the overview for the Spring Meadows Country Club project. Golf Club 3km
+        This is the overview for the Spring Meadows Country Club project. Golf Club 3km
+        .</p>
+      `
+    },
+    default: {
+        header: `No Location Found`,
+        overview: `
+          <p>No Data Found.</p>
+        `,
+        photos: `
+          <div id="carousel2" class="carousel slide">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://NoData.png" class="d-block w-100" alt="brookwood-demo">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel2" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel2" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        `,
+        details: `
+          <p>No Data Found.</p>
+        `
+      }
+  };
+  
